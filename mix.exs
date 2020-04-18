77977dae696b46c55954e840afdff8ea6271fe52
Marius Melzer <marius@rasumi.net>
2020-04-18 13:43:57 +0200

    Move from distillery to mix releases

--- mix.exs
+++ mix.exs
@@ -6,7 +6,8 @@ defmodule SignalTower.MixProject do
       app: :signal_tower,
       version: "1.1.0",
       elixir: "~> 1.9",
-      deps: deps()
+      deps: deps(),
+      releases: releases()
     ]
   end
 
@@ -23,8 +24,20 @@ defmodule SignalTower.MixProject do
     [
       {:cowboy, "~> 2.6.0"},
       {:poison, "~> 4.0.1"},
-      {:distillery, "~> 2.0.12"},
       {:prometheus_ex, "~> 3.0"}
+    ]
+  end
+
+  defp releases do
+    [
+      staging: [
+        include_executables_for: [:unix],
+        applications: [runtime_tools: :permanent]
+      ],
+      production: [
+        include_executables_for: [:unix],
+        applications: [runtime_tools: :permanent]
+      ]
     ]
   end
 end
